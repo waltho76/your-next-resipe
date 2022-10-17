1747662c0d32feb6292c6db01954ec24e035c6fb
Walter Thomas <walterdthomas@me.com>
2022-10-17 15:36:36 -0400

    Walter Thomas

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let button = document.querySelector("#searchButton")
 
-//use Event as a parameter
+
 async function getData (event) {
 event.preventDefault()
 let textInput = document.querySelector("#inputBar").value.toLowerCase()
-fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?s=${textInput}`)
+fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${textInput}`)
     .then(res => {
         return res.json()
     })
@@ -12,20 +12,18 @@ fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?s=${textInput}`)
         console.log("success!", res.meals)
         let recipeTitle = document.querySelector("#recipeTitle")
         let recipeImg = document.querySelector(`#imgIns`)
-        let recipeList = document.querySelector(`#recipeList`)
-       // let moves = document.querySelector(`#moves`)
-
-       
-        recipeTitle.innerText = res.meals.strMeal
-
-        recipeImg.innerHTML = `<img src="${res.meals.strMealThumb}"/>`
-
-        recipeList.innerHTML = ``
-        res.meals.forEach((rl) => {return recipeList.innerHTML += `<li>“${rl.meal}“</li>`})
-
+        let recipeDesc = document.querySelector(`#recipeDesc`)
+      
+       //shows category in header
+        recipeTitle.innerText = res.meals[0].strMeal
+        recipeImg.setAttribute("src", res.meals[0].strMealThumb)
+        recipeDesc.innerText=res.meals[0].strInstructions
+        
+        
         
     })
     
     
 }
+
 button.addEventListener("click", getData);
